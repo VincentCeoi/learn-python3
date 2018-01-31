351ee265fc0752a5d1fd2111432eb6866bbea509
Vincent Cui <vincent.cui@lazada.com>
2018-01-31 15:29:16 +0800

    [master] Learn the mysql connect of python

--- samples/db/do_mysql.py
+++ samples/db/do_mysql.py
@@ -9,13 +9,11 @@
 import mysql.connector
 
 # change root password to yours:
-conn = mysql.connector.connect(user='root', password='password', database='test')
+conn = mysql.connector.connect(user='root', password='rcon3#ni^ssancLe', database='inner.chen',host='127.0.0.1', port=3306)
 
 cursor = conn.cursor()
-# 创建user表:
-cursor.execute('create table user (id varchar(20) primary key, name varchar(20))')
 # 插入一行记录，注意MySQL的占位符是%s:
-cursor.execute('insert into user (id, name) values (%s, %s)', ('1', 'Michael'))
+cursor.execute('update background_task set updated_at = now() where id = 117');
 print('rowcount =', cursor.rowcount)
 # 提交事务:
 conn.commit()
@@ -23,7 +21,7 @@ cursor.close()
 
 # 运行查询:
 cursor = conn.cursor()
-cursor.execute('select * from user where id = %s', ('1',))
+cursor.execute('select * from background_task where id = %s', ('117',))
 values = cursor.fetchall()
 print(values)
 # 关闭Cursor和Connection:
